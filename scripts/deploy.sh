cd4f4710b434c356baaed41d713b34cfc354a7a6
goku <coding.game@gmail.com>
2017-02-06 16:57:21 +0800

    Add server deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-ssh root@your_server_ip ~/deploy-server.sh
+ssh root@your_server_ip << EOF
+ls -a -l
+export GOPATH=$HOME/gohome
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+"~/deploy-server.sh"
+EOF
