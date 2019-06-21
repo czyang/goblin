e9fafa9b9b3ecfd8d370cf39e3459a316cb5501b
Chengzhi Yang <coding.game@gmail.com>
2019-06-21 13:23:21 -0400

    Update vendor package

--- markdown_blog.go
+++ markdown_blog.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/coretech/igo/log"
 	"gopkg.in/russross/blackfriday.v2"
 	"html/template"
 	"io/ioutil"
@@ -13,8 +14,8 @@ func MarkdownFileToHTML(fileName string) (template.HTML, *PostMeta) {
 	jsonHeadBytes, mdBytes := ExtractMetaJSONStr(fileRead)
 	mdBytes = TrimTitleLine(mdBytes)
 	postMeta := new(PostMeta)
-	if err := json.Unmarshal(jsonHeadBytes, &postMeta); err {
-		checkError(err)
+	if err := json.Unmarshal(jsonHeadBytes, &postMeta); err != nil {
+		log.Info(fileName)
 	}
 	return template.HTML(blackfriday.Run(mdBytes)), postMeta
 }
